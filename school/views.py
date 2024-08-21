0dc486390e2b9d45ad94f4a8983c1a4375cf3d64
kisharra <djagisama123@gmail.com>
2024-08-21 10:55:31 +1000

    initial commit

---
+++ school/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    students = Student.objects.all().order_by(ordering)
+    context = {
+        'student': students
+    }
+
+    return render(request, template, context)
